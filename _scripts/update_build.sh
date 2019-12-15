b08803ea9295fc64278cb49002fc0217a29405f4
doclockh <chris@lockhartlab.org>
2019-12-14 18:56:10 -0800

    First commit!

--- _scripts/update_build.sh
+++ _scripts/update_build.sh
@@ -23,8 +23,8 @@ python _scripts/increment_version.py
 # upload to git without triggering TravisCI again
 cat version.yml
 git add version.yml
-git add izzy/_version.py
+git add simlib/_version.py
 git add docs/build/*
-git add docs/source/api/generated/*
+# git add docs/source/api/generated/*
 git commit -m "updating doc and version [ci skip]"
 git push -fq https://doclockh:"${GIT_API_KEY}"@github.com/"${TRAVIS_REPO_SLUG}".git master
