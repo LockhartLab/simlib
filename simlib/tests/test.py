5f9dba2ccf2c3b359b2cc82965502d52969d2fab
doclockh <chris@lockhartlab.org>
2019-12-14 19:10:57 -0800

    First commit!

--- simlib/tests/test.py
+++ simlib/tests/test.py
@@ -1,2 +1,36 @@
+"""
+test.py
+written in Python3
+author: C. Lockhart <chris@lockhartlab.org>
+"""
 
-assert True
+from simlib.tests import TestProtein
+
+import sys
+import unittest
+
+# Define test cases
+# TODO see if there's an easier way to look for these cases (without manually updating a list)
+test_cases = [
+    TestGeneric,
+    TestMetrics
+]
+
+
+# Function to run tests
+def test():
+    # Create a test suite
+    suite = unittest.TestSuite()
+
+    # Load all our test cases into the suite
+    loader = unittest.TestLoader()
+    for test_case in test_cases:
+        suite.addTests(loader.loadTestsFromTestCase(test_case))
+
+    # Create test runner and run
+    runner = unittest.TextTestRunner()
+    result = runner.run(suite)
+
+    # If the test was not successful, exit with a code
+    sys.exit(not result.wasSuccessful())
+
