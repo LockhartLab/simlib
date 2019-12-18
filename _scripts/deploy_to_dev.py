14a69829cc0eade7f8fa01588fa627c925dd0ccb
doclockh <chris@lockhartlab.org>
2019-12-17 23:40:30 -0800

    izzy must come last in requirements

--- _scripts/deploy_to_dev.py
+++ _scripts/deploy_to_dev.py
@@ -1,18 +1,19 @@
 """
 deploy_to_dev.py
 written in Python3
-author: C. Lockhart
+author: C. Lockhart <chris@lockhartlab.org>
 """
-
 
 from izzy.io import Git
 
-
-# Connect to repository
+# Connect to git repository
 git = Git()
+
+# We must be on master branch
+branch = git.get_branch()
+assert branch == 'dev', branch
 
 # Connect to git repository, tag, add files, commit, push
-git = Git()
 git.add('-A')
 git.commit(input('Commit message: '))
 git.push(remote='origin', branch='dev')
