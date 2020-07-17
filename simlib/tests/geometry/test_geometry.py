9198e9a76b8c9d1fb04b6db9dd0818c6bc6305f6
doclockh <chris@lockhartlab.org>
2020-07-16 17:43:42 -0700

    Changed assert_equal to assert_almost_equal

--- simlib/tests/geometry/test_geometry.py
+++ simlib/tests/geometry/test_geometry.py
@@ -49,12 +49,12 @@ class TestGeometry(TestCase):
         w_norm = _norm(w, n_points)
 
         # Test normalized vectors
-        np.testing.assert_equal(u_norm.ravel(), norm(u).reshape(-1))
-        np.testing.assert_equal(v_norm.ravel(), norm(v).reshape(-1))
-        np.testing.assert_equal(w_norm.ravel(), norm(w).reshape(-1))
-        np.testing.assert_equal(u / u_norm, vector(a, b, normalize=True))
-        np.testing.assert_equal(v / v_norm, vector(b, c, normalize=True))
-        np.testing.assert_equal(w / w_norm, vector(c, d, normalize=True))
+        np.testing.assert_almost_equal(u_norm.ravel(), norm(u).reshape(-1))
+        np.testing.assert_almost_equal(v_norm.ravel(), norm(v).reshape(-1))
+        np.testing.assert_almost_equal(w_norm.ravel(), norm(w).reshape(-1))
+        np.testing.assert_almost_equal(u / u_norm, vector(a, b, normalize=True))
+        np.testing.assert_almost_equal(v / v_norm, vector(b, c, normalize=True))
+        np.testing.assert_almost_equal(w / w_norm, vector(c, d, normalize=True))
 
         # Get unit vectors
         u_unit = u / u_norm
