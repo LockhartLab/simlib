2917e1c0a8ad33c3bd95e1f28b183d07611104b9
doclockh <chris@lockhartlab.org>
2020-07-16 18:36:46 -0700

    Removing call to increment_version script from deploy_to_master.py

--- _scripts/deploy_to_master.py
+++ _scripts/deploy_to_master.py
@@ -4,7 +4,7 @@ written in Python3
 author: C. Lockhart <chris@lockhartlab.org>
 """
 
-from _scripts.increment_version import *
+# from _scripts.increment_version import *
 from gitpipe import Git
 import yaml
 
