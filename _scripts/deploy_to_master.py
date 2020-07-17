2886cea05b073872fbd02007e7a392ca14b6ed73
doclockh <chris@lockhartlab.org>
2020-07-16 18:26:53 -0700

    Trying to deploy only on successful travis test

---
+++ _scripts/deploy_to_master.py
@@ -0,0 +1,30 @@
+"""
+deploy_to_master.py
+written in Python3
+author: C. Lockhart <chris@lockhartlab.org>
+"""
+
+from _scripts.increment_version import *
+from gitpipe import Git
+import yaml
+
+# Connect to git repository
+git = Git()
+
+# We must be on master branch
+branch = git.get_branch()
+assert branch == 'master', branch
+
+# Increment version; print out string
+# version = increment_version()
+# print('package version: {}\n'.format(version))
+
+with open('version.yml', 'r') as f:
+    version = yaml.safe_load(f.read())
+version = '.'.join([str(version[key]) for key in ['major', 'minor', 'patch']])
+
+# Connect to git repository, tag, add files, commit, push
+git.add('-A')
+git.commit('[deploy] test successful')
+git.tag('v' + version)
+git.push(remote='origin', branch='master', options='--tags')
--- docs/source/api/generated/simlib.framework.Structure.rst
+++ docs/source/api/generated/simlib.framework.Structure.rst
@@ -5,3 +5,8 @@ simlib.framework.Structure
 
 .. autoclass:: Structure
    :members:
+   :inherited-members:
+   :no-undoc-members:
+
+   .. rubric:: Methods:
+   .. automethod:: __init__
